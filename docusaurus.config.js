c3eb1aa62d6f26834c95992b739eb7b8b24d0719
lovesummer718 <lovesummer718@gmail.com>
2023-07-28 21:08:43 -0300

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  deploymentBrand: 'github_pages',
+  deploymentBranch: 'github_pages',
   trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
